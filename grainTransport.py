8981ea3c7c84a31cf4ad84dc6c223d958658b01f
Abdul Haqani <ahaqa011@uottawa.ca>
2019-08-02 14:42:01 -0400

    icon works

--- grainTransport.py
+++ grainTransport.py
@@ -660,7 +660,7 @@ class grainTransport:
 
         # Declare instance attributes
         self.actions = []
-        self.menu = self.tr(u'&grain Transport Shortest Path')
+        self.menu = self.tr(u'&grain Transport')
 
         # Check if plugin was started the first time in current QGIS session
         # Must be set in initGui() to survive plugin reloads
@@ -681,93 +681,19 @@ class grainTransport:
         # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate('grainTransport', message)
 
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        # if add_to_toolbar:
-        # Adds plugin icon to Plugins toolbar
-        # self.iface.addToolBarIcon(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
 
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/grainTransport/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'grainTransport'),
-            callback=self.run,
-            parent=self.iface.mainWindow())
+        self.action = QAction(
+            QIcon(':/plugins/grainTransport/icon.png'),
+            'Grain Transport', self.iface.mainWindow())
+        # connect the action to the run method
+        self.action.triggered.connect(self.run)
 
-        self.iface.addToolBarIcon(QAction(QIcon(":/plugins/grainTransport/icon.png"), "Grain Transport Shortest Path",
-                                          self.iface.mainWindow()))
-
+        # Add toolbar button and menu item
+        self.iface.addToolBarIcon(self.action)
+        self.iface.addPluginToMenu('&Grain Transport', self.action)
         # will be set False in run()
         self.first_start = True
 
@@ -933,7 +859,7 @@ class grainTransport:
 
         # Create the dialog with elements (after translation) and keep reference
         # Only create GUI ONCE in callback, so that it will only load when the plugin is started
-        if not self.first_start:
+        if self.first_start:
             self.first_start = False
         dlg = grainTransportDialog()
 
@@ -943,7 +869,6 @@ class grainTransport:
         result = dlg.exec_()
         # See if OK was pressed
         if result:
-
             self.test()
             
 
